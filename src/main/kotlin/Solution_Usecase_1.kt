e1999d25bd090f89b446f9e4e37f2569b8bd8d6e
jemmy calak <jemmy.sapta14@gmail.com>
2023-12-14 11:55:26 +0700

    add solution usecase 1

--- src/main/kotlin/Solution_Usecase_1.kt
+++ src/main/kotlin/Solution_Usecase_1.kt
@@ -1,15 +1,16 @@
+import model.Bureau
 import model.Usecase1
 import java.io.File
 import java.util.*
+import kotlin.math.min
 
 class Solution_Usecase_1 {
     init {
         println("--------------------------Usecase 1------------------------------")
         val input = Scanner(File("src/main/resources/usecase/Use_Case_1.txt"))
-        input.useDelimiter("[-\n]")
-
         var listUsecaseData = emptyList<Usecase1>()
         var isFirstData = true
+
         while (input.hasNext()) {
             if (isFirstData) {
                 isFirstData = false
@@ -17,8 +18,57 @@ class Solution_Usecase_1 {
                 continue
             } else {
                 val dataInput = input.next()
-                val dataSplited = dataInput.split("|")
+                val dataSplited = dataInput.split(",")
+                val listBureau = dataSplited.subList(3, dataSplited.size).chunkArrayList(3)
+                val usecase1 = Usecase1(
+                    jeniskredit = dataSplited[0],
+                    kreditNo = dataSplited[1].toInt(),
+                    active = dataSplited[2],
+                    listBureau = listBureau
+                )
+
+                listUsecaseData = listUsecaseData + usecase1
             }
         }
+
+        getNumberActiveFacilities(listUsecaseData)
+    }
+
+    private fun getNumberActiveFacilities(data: List<Usecase1>) {
+        var activeFacility = listOf<Bureau>()
+        data.forEach {
+            if (it.active != "Y") return@forEach
+            it.listBureau.forEachIndexed { index, bureau ->
+                // DPD = tahunbulan01ht - tahunbulan24ht
+                val DPD = it.listBureau[index].DPD - it.listBureau[it.listBureau.lastIndex - index].DPD
+
+                // Collectability = tahunBulan01Kol - tahunBulan01Kol
+                val COL = it.listBureau[index].COL - it.listBureau[it.listBureau.lastIndex - index].COL
+                if (DPD > 30 || COL > 2) {
+                    activeFacility = activeFacility + bureau
+                }
+            }
+        }
+
+        println("Size of Active Facilities : ${activeFacility.size}")
+        println("Result of Active Facilities : $activeFacility")
+    }
+
+    private fun List<String>.chunkArrayList(chunkSize: Int): List<Bureau> {
+        val chunkList: MutableList<Bureau> = ArrayList()
+        var i = 0
+        while (i < size) {
+            val end = min((i + chunkSize).toDouble(), size.toDouble()).toInt()
+            val dataSublist = subList(i, end)
+            chunkList.add(
+                Bureau(
+                    DPD = dataSublist[0].toInt(),
+                    Year = dataSublist[1].toInt(),
+                    COL = dataSublist[2].toInt(),
+                )
+            )
+            i += chunkSize
+        }
+        return chunkList
     }
 }
--- src/main/kotlin/model/Usecase1.kt
+++ src/main/kotlin/model/Usecase1.kt
@@ -3,5 +3,12 @@ package model
 data class Usecase1(
     val jeniskredit: String,
     val kreditNo: Int,
-    val active: String
+    val active: String,
+    val listBureau: List<Bureau> = emptyList(),
+)
+
+data class Bureau(
+    val COL: Int,
+    val Year: Int,
+    val DPD: Int,
 )
